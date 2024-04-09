5f475d6b44dee6df9310c43d847e5e389a01efdf
unknown <jakub.zezula@hotmail.com>
2024-04-09 13:52:25 +0200

    Added zipped binaries

--- KonwerterTekstNaGrafika/Form1.Designer.cs
+++ KonwerterTekstNaGrafika/Form1.Designer.cs
@@ -121,7 +121,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Konwerter tekstu na grafikę";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
